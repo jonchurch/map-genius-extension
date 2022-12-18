cec5b674e4c13f299771cadbad6a538c5052fb45
jonchurch <me@jonchurch.com>
2022-12-18 15:02:34 -0500

    add setInterval loop to content script

--- src/bizspark.js
+++ src/bizspark.js
@@ -1,12 +1,14 @@
 const OWNER_SELECTOR = "#table-7 > tbody > tr:nth-child(3) > td";
 
-const ownerContent = document.querySelector(OWNER_SELECTOR);
-
-if (ownerContent) {
-  const text = ownerContent.textContent();
-  console.log({ text });
-  const SEARCH_URL = `https://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults/EntityName/${encodeURI(
-    text
-  )}/Page1`;
-  ownerContent.wrapInner(() => `<a href="${SEARCH_URL}">`);
-}
+setInterval(() => {
+  const ownerContent = document.querySelector(OWNER_SELECTOR);
+  console.log("text beep");
+  if (ownerContent) {
+    const text = ownerContent.textContent();
+    console.log({ text });
+    const SEARCH_URL = `https://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults/EntityName/${encodeURI(
+      text
+    )}/Page1`;
+    ownerContent.wrapInner(() => `<a href="${SEARCH_URL}">`);
+  }
+}, 1500);
