0c0f0f17e27ee6626affe852374f136c57416c40
jonchurch <me@jonchurch.com>
2022-12-18 15:38:06 -0500

    link to bizspark site if there is an owner visible in the tax table

--- src/bizspark.js
+++ src/bizspark.js
@@ -1,14 +1,26 @@
-const OWNER_SELECTOR = "#table-7 > tbody > tr:nth-child(3) > td";
+const OWNER_SELECTOR =
+  "#taxParcelTab > #table-7 > tbody > tr:nth-child(3) > td";
+const OWNER_LINK_SELECTOR =
+  "#taxParcelTab > #table-7 > tbody > tr:nth-child(3) > td > a";
 
 setInterval(() => {
   const ownerContent = document.querySelector(OWNER_SELECTOR);
   console.log("text beep");
   if (ownerContent) {
-    const text = ownerContent.textContent();
-    console.log({ text });
+    const linkSelect = document.querySelector(OWNER_LINK_SELECTOR);
+    console.log({ linkSelect });
+    if (linkSelect) {
+      console.log("link exists");
+      return;
+    }
+    const content = ownerContent.textContent;
+    const ownerName = content.split(" ").slice(1).join(" ");
+    console.log({ content });
     const SEARCH_URL = `https://search.sunbiz.org/Inquiry/CorporationSearch/SearchResults/EntityName/${encodeURI(
-      text
+      ownerName
     )}/Page1`;
-    ownerContent.wrapInner(() => `<a href="${SEARCH_URL}">`);
+    ownerContent.innerHTML = `<b>${
+      content.split(" ")[0]
+    }</b> <a target="_blank" rel="noopener noreferrer" href="${SEARCH_URL}">${ownerName}</a>`;
   }
 }, 1500);
